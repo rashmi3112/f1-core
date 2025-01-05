afef4a99efe8b73ad5d10fa44e1891932d0d5811
rashmi3112 <rashmiunhale12@gmail.com>
2025-01-05 14:46:33 +0530

    Fetching the product data from backend styling, filtering, sorting, searching, pagination

---
+++ Clientside/src/app/features/shop/product-item/product-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../shared/models/product';
+import { MatCardModule } from '@angular/material/card';
+import { CurrencyPipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-product-item',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    CurrencyPipe,
+    MatIconModule
+  ],
+  templateUrl: './product-item.component.html',
+  styleUrl: './product-item.component.scss'
+})
+export class ProductItemComponent {
+  @Input() product?: Product;
+}
