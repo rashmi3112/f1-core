f9c7fe2715ed90201ad112eb3afa67b9b0fbf177
rashmi3112 <rashmiunhale12@gmail.com>
2025-02-05 18:40:10 +0530

    frontend changes

--- Clientside/src/app/app.routes.ts
+++ Clientside/src/app/app.routes.ts
@@ -2,16 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ShopComponent } from './features/shop/shop.component';
 import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
-import { TestErrorComponent } from './features/test-error/test-error.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 import { CartComponent } from './features/cart/cart.component';
 import { LoginComponent } from './features/account/login/login.component';
 import { authGuard } from './core/guards/auth.guard';
 import { adminGuard } from './core/guards/admin.guard';
+import { ContactComponent } from './features/contact/contact.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'contact', component: ContactComponent},
     {path: 'shop', component: ShopComponent},
     {path: 'shop/:id', component: ProductDetailsComponent},
     {path: 'cart', component: CartComponent},
@@ -22,7 +23,6 @@ export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'account', loadChildren: () => import('./features/account/routes')
         .then(r => r.accountRoutes)},
-    {path: 'test-error', component: TestErrorComponent},
     {path: 'not-found', component: NotFoundComponent},
     {path: 'server-error', component: ServerErrorComponent},
     {path: 'admin', loadComponent: () => import('./features/admin/admin.component')
--- Clientside/src/app/features/home/home.component.ts
+++ Clientside/src/app/features/home/home.component.ts
@@ -1,15 +1,35 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    RouterLink,
+    NgFor
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  images = [
+    '../images/f1-cover.jpg',
+    '../images/f1-cover2.jpg',
+    '../images/f1-cover3.jpg',
+    '../images/f1-cover4.jpg'
+  ];
+  currentIndex = 0;
 
+  constructor() {
+    this.startSlideshow();
+  }
+
+  startSlideshow() {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 3000); // Change image every 3 seconds
+  }
 }
